b63453b27b70efc53c76218b70ea329d98202ff5
vskokan <victoriaskokan@gmail.com>
2020-04-10 13:26:54 +0300

    Fixed conjunction

--- src/ex4_js-objects-part2/task-03.js
+++ src/ex4_js-objects-part2/task-03.js
@@ -2,7 +2,7 @@ function deleteFirstAndLastSpace(string) {
 	if ((typeof string !== "string")&&(typeof string !== "object")) {
 		return undefined;
 	}
-	if ((string.charAt(0) === " ") & (string.charAt(string.length - 1) === " ")) {
+	if ((string.charAt(0) === " ") && (string.charAt(string.length - 1) === " ")) {
 		return string.slice(1,-1);
 	}
 
